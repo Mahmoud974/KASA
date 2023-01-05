dad1823f7f8b5c311e30f51e954ef1c03510c0fe
Moussa <manuscrit974@gmail.com>
2023-01-05 16:47:21 +0100

    terminer

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,10 @@ const Cards = ({ item }) => {
       <li
         className="card_item"
         style={{
-          background: `url(${item.cover})`,
+          background: `linear-gradient(
+        rgba(181, 112, 112, 0), 
+        rgba(0,0,0, 0.6)
+      ),url(${item.cover})`,
         }}
       >
         <h2>{item.title}</h2>
